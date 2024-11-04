eabdaecf115dcba2ef0355146b7c634fa3161022
Գևորգյան Վարդան Արմենի <Gevorgyan_VA@ena.am>
2024-11-04 11:13:22 +0400

    version 1.0

---
+++ index.php
@@ -0,0 +1,24 @@
+<html>
+<head>
+<style>
+body { background-color: blue }
+</style>
+</head>
+<body>
+<center>
+<font color="white">
+<h1>Hello From DevOps WebPage!</h1>
+</font>
+</center>
+
+<?php
+
+echo '<p style="color: red; text-align: center">
+    Hello From AWS Elastic Beanstalk WebServer!
+    </p>';
+
+
+echo '<br>Local IP:'.$_SERVER['SERVER_ADDR'];
+?>
+</body>
+</html>
